7656c71a3a83bab39c4b7f04864dfac0247be1a2
FabCode67 <mwanafunzifabrice@gmail.com>
2025-04-11 11:59:49 +0200

    quick fix

--- components/layouts/Dashboardlayout.tsx
+++ components/layouts/Dashboardlayout.tsx
@@ -2,7 +2,7 @@
 'use client';
 
 import React, {  useState } from 'react';
-import { useRouter } from 'next/navigation';
+// import { useRouter } from 'next/navigation';
 import Sidebar from './Sidebar';
 import Header from './Header';
 
